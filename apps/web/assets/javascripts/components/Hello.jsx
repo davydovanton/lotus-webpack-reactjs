2c8e0a8e4512e7a6152dfdc9ce748c9d6be3de01
Anton Davydov <antondavydov.o@gmail.com>
2015-12-28 20:32:29 +0300

    Update

--- apps/web/assets/javascripts/components/Hello.jsx
+++ apps/web/assets/javascripts/components/Hello.jsx
@@ -1,9 +1,54 @@
 'use strict'
-var React = require('react')
+import React from 'react';
+
+import { Paper, GridList, GridTile, IconButton, StarBorder } from 'material-ui';
+
 module.exports = React.createClass({
   displayName: 'HelloReact',
 
   render: function(){
-    return <div>Hello React</div>
+    return (
+      <GridList
+      cols={3}
+      cellHeight={340}
+      style={{overflowY: 'auto'}}>
+        <GridTile>
+          <Paper zDepth={1} style={{width: 300, height: 300, 'margin-left': 20}}>
+            <p>zDepth=1</p>
+          </Paper>
+        </GridTile>
+
+        <GridTile>
+          <Paper zDepth={1} style={{width: 300, height: 300, 'margin-left': 20}}>
+            <p>zDepth=1</p>
+          </Paper>
+        </GridTile>
+
+        <GridTile>
+          <Paper zDepth={1} style={{width: 300, height: 300, 'margin-left': 20}}>
+            <p>zDepth=1</p>
+          </Paper>
+        </GridTile>
+
+        <GridTile>
+          <Paper zDepth={1} style={{width: 300, height: 300, 'margin-left': 20}}>
+            <p>zDepth=1</p>
+          </Paper>
+        </GridTile>
+
+        <GridTile>
+          <Paper zDepth={1} style={{width: 300, height: 300, 'margin-left': 20}}>
+            <p>zDepth=1</p>
+          </Paper>
+        </GridTile>
+
+        <GridTile>
+          <Paper zDepth={1} style={{width: 300, height: 300, 'margin-left': 20}}>
+            <p>zDepth=1</p>
+          </Paper>
+        </GridTile>
+
+      </GridList>
+    );
   }
 })
